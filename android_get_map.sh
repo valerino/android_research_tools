f4d6199f6078d7c6ce356987cae6c263cc43fa91
valerino <valerio.lupi@cy4gate.com>
2023-05-30 20:49:13 +0200

    fixes

--- android_get_map.sh
+++ android_get_map.sh
@@ -3,11 +3,14 @@ source ./shared.sh
 
 function usage {
     echo "get /proc/pid/maps giving a process/package name."
-    echo "usage: $0 <-p process/package_name> [-d device serial]"
+    echo "usage: $0 <-p process/package_name> [-d device serial] [-o to get pid only]"
 }
 
-while getopts "d:p:" arg; do
+while getopts "d:op:" arg; do
     case $arg in
+    o)
+        _PIDONLY=1
+        ;;
     p)
         _PACKAGE="${OPTARG}"
         ;;
@@ -37,5 +40,9 @@ if [ "$_r" -eq 1 ]; then
     echo "ERROR!"
     exit 1
 fi
+if [ ! -z "$_PIDONLY" ]; then
+    exit 0
+fi
+
 _pid=$(get_result_from_lfs_string "$_res" 1)
 run_adb shell su -c "cat /proc/$_pid/maps"
