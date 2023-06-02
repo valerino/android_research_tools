8975c1a212af6d203ecf64cb260ac7b281ca59e3
valerino <valerio.lupi@cy4gate.com>
2023-06-02 23:02:45 +0200

    fixes

--- android_get_address.sh
+++ android_get_address.sh
@@ -3,11 +3,11 @@ source "$(dirname $(realpath $0))/shared.sh"
 
 function usage {
     echo "resolve offset or export in process, return address, pid, base"
-    echo "usage: $0 <-p process> <-m modulename> <-a offset as 0xOFFSETFROMBASE|exportname> [-d device serial]"
+    echo "usage: $0 <-p process> <-m modulename> <-a offset as 0xOFFSETFROMBASE|exportname> [-d device serial] [-r to restart frida-server]"
     echo "-p, -m, -d can also be defined as _PROCESS, _MODULE, _DEVICE environment variables, respectively."
 }
 
-while getopts "p:a:m:d:" arg; do
+while getopts "p:a:m:d:r" arg; do
     case $arg in
     p)
         _PROCESS="${OPTARG}"
@@ -20,6 +20,9 @@ while getopts "p:a:m:d:" arg; do
         ;;
     d)
         _DEVICE="${OPTARG}"
+        ;;
+    r)
+        _RESTART=1
         ;;
     *)
         usage "$0"
@@ -37,7 +40,9 @@ set -- "$(get_absolute_script_parent_path)/frida_run_script.py"
 if [ ! -z "$_DEVICE" ]; then
     set -- "$@" --device $_DEVICE
 fi
-
+if [ ! -z "$_RESTART" ]; then
+    set -- "$@" --frida_restart
+fi
 set -- "$@" --package_name "$_PROCESS" --js_path "$(get_absolute_script_parent_path)/frida_get_address.js"
 _PARAMS="{\"module\":\"$_MODULE\", \"offset\":\"$_ADDRESS\"}"
 set -- "$@" --parameters
